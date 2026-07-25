2e83a3f2f178569f195c2c7e9ff7e452af79fcda
Enzo Ariel Sanchez <enzo.sanchez.ariel@gmail.com>
2026-07-25 18:46:40 -0300

    Agregar archivos de proyecto.

---
+++ Domain.Model/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+
+        // INPROGRESS, CANCELLED, DELIVERED, PAID
+        public string State { get; set; }
+        public User Client { get; set; }
+        public List<Table> Table { get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ Domain.Model/Price.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Model {
+    public class Price {
+        public decimal Value { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
